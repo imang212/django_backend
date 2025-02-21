8a0a3d6a50b3a726e6c463b4e247e14f99ac854a
Patrik Poklop <151776170+imang212@users.noreply.github.com>
2025-02-21 09:43:32 +0100

    Aktualizace ukolu

--- todo_project/tasks/models.py
+++ todo_project/tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     description = models.TextField(max_length=500, blank=True, null=True)
     due_date = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to="tasks_photos/", blank=True, null=True)
--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+@api_view(['GET', 'POST'])
+def task_list(request):
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
